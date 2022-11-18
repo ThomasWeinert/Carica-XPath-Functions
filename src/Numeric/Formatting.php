9742a37b629244ed5aa36c4ddb25c8bc5d6faeb9
Thomas Weinert <thomas@weinert.info>
2022-11-18 10:41:32 +0100

    [FEATURE] Add Extended Integer Formatting
    * ASCII Letter sequences: A B ... AA AB ...
    * Roman numbers
    * Words - lowercase, uppercase, capitalized

--- src/Numeric/Formatting.php
+++ src/Numeric/Formatting.php
@@ -9,6 +9,10 @@ namespace Carica\XPathFunctions\Numeric {
 
   abstract class Formatting {
 
+    private const IGNORE = [
+      'de' => ['und']
+    ];
+
     /**
      * @param float $input
      * @param string $picture
@@ -17,25 +21,107 @@ namespace Carica\XPathFunctions\Numeric {
      * @throws XpathError
      */
     public static function formatInteger(float $input, string $picture, string $language): string {
-      return self::createNumberFormatter($language, $picture)->format((int)$input);
-    }
-
-    /**
-     * @param string $language
-     * @param string $pattern
-     * @return NumberFormatter
-     * @throws XpathError
-     */
-    private static function createNumberFormatter(string $language, string $pattern): NumberFormatter {
-      /* @todo Add some real parsing for the pattern */
-      if ($pattern === 'w') {
-        return NumberFormatter::create($language, NumberFormatter::SPELLOUT);
+      switch ($picture) {
+      case 'A':
+        return self::toSequenceValue((int) $input, 'ABCDEFGHIJKLMNOPQRTSUVWXYZ');
+      case 'a':
+        return self::toSequenceValue((int) $input, 'abcdefghijklmnopqrtsuvwxyz');
+      case 'I':
+        return self::toRomanNumber((int) $input);
+      case 'i':
+        return strtolower(self::toRomanNumber((int) $input));
+      case 'w':
+        $formatter = NumberFormatter::create($language, NumberFormatter::SPELLOUT);
+        return \Carica\XPathFunctions\Strings\Values::lowerCase(
+          $formatter->format((int)$input)
+        );
+      case 'W':
+        $formatter = NumberFormatter::create($language, NumberFormatter::SPELLOUT);
+        return \Carica\XPathFunctions\Strings\Values::upperCase(
+          $formatter->format((int)$input)
+        );
+      case 'Ww':
+        $formatter = NumberFormatter::create($language, NumberFormatter::SPELLOUT);
+        return self::capitalizeNumberWords($formatter->format((int)$input), $language);
       }
-      $formatter = NumberFormatter::create($language, NumberFormatter::DECIMAL);
-      if (!$formatter->setPattern($pattern)) {
+      $hasDigit = preg_match('(\\p{Nd})u', $picture);
+      if ($hasDigit && !preg_match('(^((\\p{Nd}|#|[^\\p{N}\\p{L}])+?)$)Du', $picture)) {
+        throw new XpathError(
+          Namespaces::XMLNS_ERR.'#FODF1310',
+          ' Invalid decimal format picture string'
+        );
+      }
+      $formatter = NumberFormatter::create($language, NumberFormatter::PATTERN_DECIMAL);
+      if (!$formatter->setPattern($picture)) {
         throw new XpathError(Namespaces::XMLNS_ERR.'#FODF1310', 'Invalid decimal format picture string.');
       }
-      return $formatter;
+      return $formatter->format((int)$input);
+    }
+
+    private static function toSequenceValue(int $input, string $sequence): string {
+      $isNegative = $input < 0;
+      $result = '';
+      $current = abs($input);
+      $sequenceLength = strlen($sequence);
+      if ($current <= $sequenceLength) {
+        $result = $sequence[$current - 1];
+      } else {
+        while ($current > 0) {
+          $result = $sequence[(--$current) % $sequenceLength].$result;
+          $current = (int)($current / $sequenceLength);
+        }
+      }
+      return $isNegative ? '-'.$result : $result;
+    }
+
+    private static function toRomanNumber(int $input): string {
+      $map = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+      ];
+      $result = '';
+      while ($input > 0) {
+        foreach ($map as $roman => $value) {
+          if($input >= $value) {
+            $input -= $value;
+            $result .= $roman;
+            break;
+          }
+        }
+      }
+      return $result;
+    }
+
+    private static function capitalizeNumberWords(bool|string $input, string $language) {
+      $ignoreWords = self::IGNORE[$language] ?? [];
+      $transliterator = \Transliterator::create('Any-Upper');
+      return preg_replace_callback(
+        "(
+            (?<prefix>^|[\u{00AD}\\s-])
+            (?<word>
+              (?<first_char>[^\u{00AD}\\s])
+              (?<chars>[^\u{00AD}\\s]*)
+            )
+          )ux",
+        static function($match) use ($transliterator, $ignoreWords): string {
+          if (in_array($match['word'], $ignoreWords, TRUE)) {
+            return $match[0];
+          }
+          return $match['prefix'].$transliterator->transliterate($match['first_char']).$match['chars'];
+        },
+        $input
+      );
     }
   }
 }
--- tests/Numeric/FormattingTest.php
+++ tests/Numeric/FormattingTest.php
@@ -19,8 +19,19 @@ namespace Carica\XPathFunctions\Numeric {
      * @param string $language
      * @testWith
      *   ["0123", 123, "0000"]
+     *   ["c", 3, "a"]
+     *   ["aa", 27, "a"]
+     *   ["ab", 28, "a"]
+     *   ["C", 3, "A"]
+     *   ["AA", 27, "A"]
+     *   ["iii", 3, "i"]
+     *   ["XXVII", 27, "I"]
      *   ["one hundred twenty-three", 123, "w", "en"]
+     *   ["ONE HUNDRED TWENTY-THREE", 123, "W", "en"]
+     *   ["One Hundred Twenty-three", 123, "Ww", "en"]
      *   ["ein\u00ADhundert\u00ADdrei\u00ADund\u00ADzwanzig", 123, "w", "de"]
+     *   ["Ein\u00ADHundert\u00ADDrei\u00ADund\u00ADZwanzig", 123, "Ww", "de"]
+     *   ["一百二十三", 123, "W", "zh"]
      */
     public function testFormatIntegerTroughStylesheet(
       string $expected, float $input, string $picture, string $language = ''
